22cdb6aa86529366e1f2f307f2952154e98c4955
ExjeetzZ <pushpgill2@gmail.com>
2025-05-01 22:20:11 -0700

    Basic_HTML_and_CSS Folder Assignment
    Minimum project requirements:
    
    1. Your code must include an <ol> tag
    2. Your code must include an <ul> tag
    3. Your code must style <ol> and <ul> elements with CSS
    4. You must add a character to your code using either &entity_name or &entity_number
    5. Your code must use the text-shadow property to add a shadow behind an element
    
    All the requirements have been completed.

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,5 @@
+alert("Oh so this what those scam websites use, Good to know");
+function Function_one() {
+				var string = "Guess what?";
+				var example = string.fontcolor ("red") ; 
+				document.getElementById("example").innerHTML = "Click here was clicked";}
